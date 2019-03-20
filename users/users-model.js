978256512c842cce6631967bd9caebc4ce1aced5
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-03-20 16:05:39 -0700

    renamed dbConfig.js file to users-model.js and moved it into users folder, defined db and exported, also created a find() function inside the file

---
+++ users/users-model.js
@@ -0,0 +1,28 @@
+const knex = require("knex");
+const knexConfig = require("../knexfile.js");
+
+const db = knex(config.development);
+
+module.exports = {
+  find,
+  findById,
+  add,
+  remove,
+  update,
+  findUserPosts,
+  findPostsById,
+  addPost
+};
+
+function find(query) {
+  let { page = 1, limit = 5, sortby = "id", sortdir = "asc" } = query;
+
+  const offset = limit * (page - 1);
+
+  let rows = db("users")
+    .orderBy(sortby, sortdir)
+    .limit(limit)
+    .offset(offset);
+
+  return rows;
+}
